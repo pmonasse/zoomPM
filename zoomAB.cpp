73bb7adf33d5148d441c8d29de654eecaa80f55a
Pascal Monasse <monasse@imagine.enpc.fr>
2023-07-31 15:28:01 +0200

    Copyright information

--- zoomAB.cpp
+++ zoomAB.cpp
@@ -1,3 +1,26 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/**
+ * @file zoomAB.cpp
+ * @brief Super-resolution with Perona-Malik diffusion (Belahmidi's algorithm)
+ * 
+ * Copyright (c) 2004 Abdelmounim Belahmidi
+ * Copyright (c) 2023 Pascal Monasse
+ * All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "cmdLine.h"
 #include "io_png.h"
 #include <iostream>
