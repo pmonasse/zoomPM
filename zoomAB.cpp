7513a4bf4c8c2b43d1d7e6f60f542c889960753b
Pascal Monasse <pascal.monasse@enpc.fr>
2024-02-26 15:02:47 +0100

    Use padding to avoid separately handling borders in derivatives

--- zoomAB.cpp
+++ zoomAB.cpp
@@ -57,6 +57,12 @@ const float sqrt22=sqrt2/2;
 const float minGrad=0.001f; ///< Minimum gradient norm for applying anisotropy
 const float dt = 0.1f; ///< Time step for diffusion
 
+/// Clamp \a f in interval [0,255].
+void clamp(float& f) {
+    if(f<0.0f) f=0.0f;
+    else if(f>255.0f) f=255.0f;
+}
+
 /// Compute the 0-order spline interpolation of the input image
 void zoom_duplication(const float* in, int w, int h, int z, float* out) {
     const int zw=z*w;
@@ -70,8 +76,21 @@ void zoom_duplication(const float* in, int w, int h, int z, float* out) {
     }
 }
 
+/// Image \a in is of actual size (zw+2)x(zh+2) because it has a padding of
+/// 1 pixel each side: fill this padding.
+void fill_padding(float* in, int zw, int zh) {
+    for(int i=1; i<=zh; i++) {
+        int ii = i*(zw+2);
+        in[ii]      = in[ii+1];  // Pad left
+        in[ii+zw+1] = in[ii+zw]; // Pad right
+    }
+    memcpy(in,               in+zw+2,      (zw+2)*sizeof(float)); // Pad top
+    memcpy(in+(zw+2)*(zh+1), in+(zw+2)*zh, (zw+2)*sizeof(float)); // Pad bottom
+}
+
 /// Replace each block of \a z x \a z pixels in \a in by its average.
 /// Write the result in \a average.
+/// Warning: image \a im is padded by 1 pixel each side. **TODO**
 void projectionPU(const float*  in, int w, int h, int z,
                   float* temp, float* average) {
     float norm = 1.0f/float(z*z);
@@ -81,7 +100,7 @@ void projectionPU(const float*  in, int w, int h, int z,
             float s=0.0;
             for(int y=j*z; y<(j+1)*z; y++)
                 for(int x=i*z; x<(i+1)*z; x++){
-                    int Zoffset = z*w*y+x;
+                    int Zoffset = (z*w+2)*(y+1)+(x+1);
                     s += in[Zoffset];
                 }
             s *= norm;
@@ -124,76 +143,21 @@ void derivatives(const float a0[3], const float a1[3], const float a2[3],
         l4*(a2[2]+a0[0]) + l3*(a0[2]+a2[0]); 
 }
 
-void compute_derivatives(const float *im, int w, int dy,
-                         float *grad, float *evolxixi, float *evolnn) {
+/// Warning: image \a im is padded by 1 pixel each side.
+void compute_derivatives(const float* im, int w, int h,
+                         float* grad, float* evolxixi, float* evolnn) {
     // Inside
-    const int jmax = dy-1;
+    const int w2 = w+2;
 #ifdef _OPENMP
 #pragma omp parallel for
 #endif
-    for(int j=1; j<jmax; j++) {
-        const float *a0=im+(j-1)*w, *a1=a0+w, *a2=a1+w;     
-        float *gg=grad+j*w+1, *g=evolxixi+j*w+1, *h=evolnn+j*w+1;
-        for(int i=1; i+1<w; i++) {
+    for(int j=0; j<h; j++) {
+        const float *a0=im+j*w2, *a1=a0+w2, *a2=a1+w2;
+        float *gg=grad+j*w, *g=evolxixi+j*w, *h=evolnn+j*w;
+        for(int i=0; i<w; i++) {
             derivatives(a0,a1,a2, *gg, *g, *h);
             a0++; a1++; a2++; gg++; g++; h++;
         }
-    }
-    // Top boundary
-    const float *a0=im, *a1=im+w;
-    float *gg=grad, *g=evolxixi, *h=evolnn;
-    for(int k=1; k+1<w; k++) {
-        derivatives(a0, a0, a1, gg[k], g[k], h[k]);
-        a0++; a1++;
-    }
-    // Bottom boundary
-    a0=im+w*(dy-2); a1=a0+w;
-    gg=grad+w*(dy-1); g=evolxixi+w*(dy-1); h=evolnn+w*(dy-1);
-    for(int k=1; k+1<w; k++) {
-        derivatives(a0, a1, a1, gg[k], g[k], h[k]);
-        a0++; a1++;
-    }
-    // Left boundary
-    gg=grad+w; g=evolxixi+w; h=evolnn+w;
-    for(int k=1; k+1<dy; k++) {
-        float t0[3]={im[0+(k-1)*w], im[0+(k-1)*w], im[1+(k-1)*w]};
-        float t1[3]={im[0+(k+0)*w], im[0+(k+0)*w], im[1+(k+0)*w]};
-        float t2[3]={im[0+(k+1)*w], im[0+(k+1)*w], im[1+(k+1)*w]};
-        derivatives(t0, t1, t2, *gg, *g, *h);
-        gg+=w; g+=w; h+=w;
-    }
-    // Right boundary
-    gg=grad+2*w-1; g=evolxixi+2*w-1; h=evolnn+2*w-1;
-    for(int k=1; k+1<dy; k++) {
-        float t0[3]={im[w-2+(k-1)*w], im[w-1+(k-1)*w], im[w-1+(k-1)*w]};
-        float t1[3]={im[w-2+(k+0)*w], im[w-1+(k+0)*w], im[w-1+(k+0)*w]};
-        float t2[3]={im[w-2+(k+1)*w], im[w-1+(k+1)*w], im[w-1+(k+1)*w]};
-        derivatives(t0, t1, t2, *gg, *g, *h);
-        gg+=w; g+=w; h+=w;
-    }
-    // Corners
-    { // Top-left
-        float t0[3]={im[0], im[0], im[1]};
-        float t1[3]={im[w],im[w],im[w+1]};
-        derivatives(t0, t0, t1, grad[0], evolxixi[0], evolnn[0]);
-    }
-    { // Top-right
-        int i = w-1;
-        float t0[3]={im[i-1],   im[i],   im[i]};
-        float t1[3]={im[i-1+w],im[i+w],im[i+w]};
-        derivatives(t0, t0, t1, grad[i], evolxixi[i], evolnn[i]);
-    }
-    { // Bottom-left
-        int i = (dy-1)*w;
-        float t0[3]={im[i-w],im[i-w],im[i-w+1]};
-        float t1[3]={im[i],   im[i],   im[i+1]};
-        derivatives(t0, t1, t1, grad[i], evolxixi[i], evolnn[i]);
-    }
-    { // Bottom-right
-        int i = w*dy-1;
-        float t0[3]={im[i-w-1],im[i-w],im[i-w]};
-        float t1[3]={im[i-1],   im[i],   im[i]};
-        derivatives(t0, t1, t1, grad[i], evolxixi[i], evolnn[i]);
     }
 }
 
@@ -207,40 +171,47 @@ void zoomAB(const float* lr, int w, int h, int z, float* hr) {
     float* uxixi   = new float[iSizeImageZoom];
     float* unn     = new float[iSizeImageZoom];
     float* average = new float[iSizeImageZoom];
+    float* hrpad   = new float[iSizeImageZoom+2*(zw+zh+2)]; // Pad=1 each side
 
     const int n_iter = int(z*z/::dt+0.5f);
     std::cout << "Iterations: " << n_iter << "." << std::endl;
-    
 
     // Generate dup, the 0-order spline interpolation of the input image
     zoom_duplication(lr,w,h, z, dup);
-    // Copy it as initial zoomed image hr
-    memcpy(hr, dup, iSizeImageZoom*sizeof(float));
+    // Copy it as initial zoomed image in padded image hrpad
+    for(int i=0; i<zh; i++)
+        memcpy(hrpad+(i+1)*(zw+2)+1, dup+i*zw, zw*sizeof(float));
 
     for(int k=0; k<n_iter; k++) {
-        compute_derivatives(hr,zw,zh, grad, uxixi, unn);
-        projectionPU(hr,w,h, z, temp, average);
+        fill_padding(hrpad, zw, zh);
+        compute_derivatives(hrpad,zw,zh, grad, uxixi, unn);
+        projectionPU(hrpad,w,h, z, temp, average);
 
 #ifdef _OPENMP
 #pragma omp parallel for
 #endif
-        for(int i=0; i<iSizeImageZoom; i++) {
-            float reac = average[i] - dup[i];
-            float evolx = uxixi[i];
-            float evoln = unn[i] / (1.0f + 0.01f * grad[i]);
-            hr[i] += ::dt*(evolx + evoln - reac);
-            if(hr[i] > 255.0)
-                hr[i] = 255;
-            else if(hr[i] <0.0)
-                hr[i] = 0;
-        }
+        for(int y=0; y<zh; y++)
+            for(int x=0; x<zw; x++) {
+                int i=y*zw+x;
+                float reac = average[i] - dup[i];
+                float evolx = uxixi[i];
+                float evoln = unn[i] / (1.0f + 0.01f * grad[i]);
+                hrpad[(y+1)*(zw+2)+(x+1)] += ::dt*(evolx + evoln - reac);
+                clamp(hrpad[(y+1)*(zw+2)+(x+1)]);
+            }
     }
+
+    // Remove padding
+    for(int i=0; i<zh; i++)
+        memcpy(hr+i*zw, hrpad+(i+1)*(zw+2)+1, zw*sizeof(float));
+
     delete [] temp;
     delete [] dup;
     delete [] uxixi;
     delete [] unn;
     delete [] grad;
     delete [] average;
+    delete [] hrpad;
 }
 
 int main(int argc, char* argv[]) {
