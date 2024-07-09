282ce73639314c7260ee362d1a000d396f822f46
Pascal Monasse <pascal.monasse@enpc.fr>
2024-07-09 09:48:40 +0200

    Debug runner script

--- .ipol/run_zoomPM.sh
+++ .ipol/run_zoomPM.sh
@@ -5,8 +5,8 @@
 
 zoom=$1
 color=$2
-color=${color/True/-c}
-color=${color/False/}
+color=${color/true/-c}
+color=${color/false/}
 
 echo "$bin/build/zoomPM -z $zoom $color input_0.png zoom.png" > demo_failure.txt
 #$bin/build/zoomPM -z $zoom $color input_0.png zoom.png
